eda18d944bb12f3fd3d3710d5ece64c672b8a54e
Kate Howard <k.l.howard.1992@gmail.com>
2019-10-03 11:56:04 +0100

    Updates

---
+++ precache-manifest.63f989988dc6fe8c0c19cce63c1e00ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47ec2955218141baec568fe73400887",
+    "url": "/noughts-and-crosses/index.html"
+  },
+  {
+    "revision": "9f7c9e89f88083bcd06a",
+    "url": "/noughts-and-crosses/static/css/main.390b3512.chunk.css"
+  },
+  {
+    "revision": "08c3ef8f5e4d910e5a68",
+    "url": "/noughts-and-crosses/static/js/2.15b92679.chunk.js"
+  },
+  {
+    "revision": "9f7c9e89f88083bcd06a",
+    "url": "/noughts-and-crosses/static/js/main.d16665db.chunk.js"
+  },
+  {
+    "revision": "43c83ab49697412a9d81",
+    "url": "/noughts-and-crosses/static/js/runtime~main.2782f76b.js"
+  }
+]);
