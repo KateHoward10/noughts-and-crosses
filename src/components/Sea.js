5ae4faaf11d253e69f32613b30fb06036efb1d4f
Kate Howard <k.l.howard.1992@gmail.com>
2019-10-07 22:47:10 +0100

    Set up ships

--- src/components/Sea.js
+++ src/components/Sea.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import Tile from './Tile';
 
 function Sea() {
+  const tiles = Array.from(Array(8), () => new Array(8).fill(''));
   const tileSideLength = Math.min(window.innerWidth, window.innerHeight) / 9;
+  const ships = {
+    a: [[0, 0, 0], [0, 'a', 0], [0, 0, 0]],
+    b: [[0, 0, 0, 0], [0, 'b', 'b', 0], [0, 0, 0, 0]],
+    c: [[0, 0, 0, 0, 0], [0, 'c', 'c', 'c', 0], [0, 0, 0, 0, 0]],
+    d: [[0, 0, 0, 0, 0, 0], [0, 'd', 'd', 'd', 'd', 0], [0, 0, 0, 0, 0, 0]]
+  };
 
   return (
     <div className="console">
@@ -14,9 +21,7 @@ function Sea() {
             gridTemplateRows: `repeat(8, ${tileSideLength}px)`
           }}
         >
-          {Array.from(Array(64).keys()).map(key => (
-            <Tile index={key} />
-          ))}
+          {tiles.map(row => row.map((tile, index) => <Tile index={index}>{tile}</Tile>))}
         </div>
       </div>
       <div className="controls"></div>
