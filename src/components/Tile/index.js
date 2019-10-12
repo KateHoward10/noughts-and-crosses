23bc0217844cef6835a812d833ccce11e774fe41
Kate Howard <k.l.howard.1992@gmail.com>
2019-10-12 17:19:39 +0100

    Display corners in right places

--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
-import { SeaTile } from './styles';
+import { SeaTile, BitOfShip } from './styles';
 
 function Tile({ ships, tile, selectingWater, initialValue }) {
   const thisShip = ships.find(ship => ship.includes(tile));
+  const indexInShip = thisShip ? thisShip.indexOf(tile) : null;
+  const solo = thisShip && thisShip.length === 1;
+  const position = thisShip
+    ? indexInShip === 0
+      ? 'start'
+      : indexInShip === thisShip.length - 1
+      ? 'end'
+      : 'middle'
+    : null;
   const [selected, setSelected] = useState(initialValue);
+
+  function getDirection() {
+    if (position === 'start') {
+      return thisShip[indexInShip + 1] - indexInShip === 7 ? 'vertical' : 'horizontal';
+    } else if (position === 'end') {
+      return indexInShip - thisShip[indexInShip - 1] === 7 ? 'vertical' : 'horizontal';
+    }
+    return null;
+  }
 
   function setTile() {
     if (initialValue) return;
@@ -14,7 +32,13 @@ function Tile({ ships, tile, selectingWater, initialValue }) {
     }
   }
 
-  return <SeaTile onClick={setTile} selected={initialValue || selected} />;
+  return (
+    <SeaTile onClick={setTile} selected={initialValue || selected}>
+      {(selected === 'ship' || initialValue === 'ship') && (
+        <BitOfShip position={position} direction={getDirection()} solo={solo} />
+      )}
+    </SeaTile>
+  );
 }
 
 export default Tile;
--- src/components/Tile/styles.js
+++ src/components/Tile/styles.js
@@ -1,7 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const SeaTile = styled.div`
-  background-color: ${props =>
-    props.selected === 'water' ? 'blue' : props.selected === 'ship' ? 'yellow' : 'transparent'};
+  background-color: ${props => (props.selected ? 'blue' : 'transparent')};
   border: 1px solid black;
+`;
+
+export const BitOfShip = styled.div`
+  background-color: yellow;
+  width: 100%;
+  height: 100%;
+  border-top-left-radius: ${props => (props.position === 'start' ? '50%' : 0)};
+  border-top-right-radius: ${props =>
+    (props.position === 'start' && props.direction === 'vertical') ||
+    (props.position === 'end' && props.direction === 'horizontal')
+      ? '50%'
+      : 0};
+  border-bottom-left-radius: ${props =>
+    (props.position === 'start' && props.direction === 'horizontal') ||
+    (props.position === 'end' && props.direction === 'vertical')
+      ? '50%'
+      : 0};
+  border-bottom-right-radius: ${props => (props.position === 'end' ? '50%' : 0)};
+  ${props =>
+    props.solo &&
+    css`
+      border-radius: 50%;
+    `}
 `;
